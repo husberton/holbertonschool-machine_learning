5b22d6691acc34140833adb86a881372b484670a
Huseyn Huseynli <12683@holbertonstudents.com>
2026-01-07 15:34:11 +0400

    Task 0: Convert NumPy array to a Pandas DataFrame

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import pandas as pd
+""" This module uses pandas"""
 
 
 def from_numpy(array):
+    """ This function comver numpy array into pandas dataframe"""
     letters = [chr(ord('A') + i) for i in range(array.shape[1])]
     return pd.DataFrame(array, columns=letters)
