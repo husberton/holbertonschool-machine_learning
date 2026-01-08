52eae450644ec351b8326f70f3e3e816a6181b32
Huseyn Huseynli <12683@holbertonstudents.com>
2026-01-08 14:26:13 +0000

    Task 3: Rename and update columns

--- pipeline/pandas/3-rename.py
+++ pipeline/pandas/3-rename.py
@@ -4,6 +4,14 @@ import pandas as pd
 
 
 def rename(df):
+     '''
+    Renames Timestamp to Datetime, converts to datetime,
+    and keeps only Datetime and Close columns.
+
+    :param df: pd.DataFrame
+    :return: modified pd.DataFrame
+    '''
     df = df.rename(columns={"Timestamp": "Datetime"})
     df["Datetime"] = pd.to_datetime(df["Datetime"], unit=s)
-    return df[["Datetime", "Close"]]
+    df = df[["Datetime", "Close"]]
+    return df
