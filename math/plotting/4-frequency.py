de4157f496707eff4ae99831699a1ba0c0dc9556
Huseyn Huseynli <12683@holbertonstudents.com>
2026-01-10 13:20:19 -0400

    Task 4: Plot a histogram

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,18 +1,22 @@
-#!/usr/bin/env python3
-""" Plot a histogram """
+!/usr/bin/env python3
+"""Docstring for 4-frequency.py."""
 import numpy as np
+
+
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """ Histogram with bins of size 10 """
+    """Docstring for frequency."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
+    plt.xticks(np.arange(0, 101, 10))
+    plt.yticks(np.arange(0, 31, 5))
+    plt.title("Project A")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
-    plt.title("Project A")
-    plt.ylim(0, 30)
-    bounds = np.arange(0, 101, 10)
-    plt.xticks(bounds)
-    plt.hist(student_grades, range=(0, 100), bins=bounds, edgecolor="black")
+    bins = np.arange(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor="black")
