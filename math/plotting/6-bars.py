eafb985c23475a20b5097a2048b1d22f0f3e1fb4
Huseyn Huseynli <12683@holbertonstudents.com>
2026-01-11 14:29:29 +0400

    Task 6: Stacked bar chart

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Plot a bar graph with 4 levels of stacked bars"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """Plot a bar graph with 4 levels of stacked bars."""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    categories = ["Farrah", "Fred", "Felicia"]
+    level1 = fruit[0]
+    level2 = fruit[1]
+    level3 = fruit[2]
+    level4 = fruit[3]
+    plt.figure(figsize=(6.4, 4.8))
+    plt.title("Number of Fruit per Person")
+    plt.ylabel("Quantity of Fruit")
+    plt.ylim(0, 80)
+    list1 = level1
+    list2 = level1 + level2
+    list3 = level1 + level2 + level3
+    plt.bar(categories, level1, color="red", width=0.5)
+    plt.bar(categories, level2, bottom=list1, color="yellow", width=0.5)
+    plt.bar(categories, level3, bottom=list2, color="#ff8000", width=0.5)
+    plt.bar(categories, level4, bottom=list3, color="#ffe5b4", width=0.5)
+    plt.legend(["apples", "bananas", "oranges", "peaches"])
